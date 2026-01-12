f25f74ca8eb73d3bc56b85d85cfea4ba91b59418
pttnguyet73 <pnguyet1992@gmail.com>
2026-01-12 21:41:06 +0700

    change api

--- Source/frontend/src/lib/api.ts
+++ Source/frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'http://localhost:8001/api/',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
